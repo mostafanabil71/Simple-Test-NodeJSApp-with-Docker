b7df29c8befa61294d5bc015c7899c8b7ff2509e
mostafanabil71 <mostafa.nabile.71@gmail.com>
2024-06-19 08:24:40 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:10-alpine
+
+#By default, the Docker Node image includes a non-root node user that you can use to avoid running your 
+#application container as root. (It is a recommended security practice to avoid running containers as root)
+
+#We will therefore use the node user’s home directory as the working directory for our application and set them
+#as our user inside the container. 
+
+#To fine-tune the permissions on our application code in the container, let’s create the node_modules subdirectory
+#in /home/node along with the app directory. (to ensure  that they have the permissions we want)
+#In addition to creating these directories, we will set ownership on them to our node user:
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+
+WORKDIR /home/node/app
+
+COPY package*.json ./
+
+USER node
+
+RUN npm install
+
+COPY --chown=node:node . .
+
+EXPOSE 8080
+
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,33 @@
+//The first part of the file will create the Express application and Router objects,and 
+// define the base directory and port as constants:
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+//This section of the file also sets a couple of constants, path and port:
+//So, lets set the routes for the application using the router object:
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/devops', function(req,res){
+  res.sendFile(path + 'devops.html');
+});
+
+//Finally, mount the router middleware and the application’s static assets and tell the app to listen on port 8080:
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('app is listening on port 8080!')
+})
+
